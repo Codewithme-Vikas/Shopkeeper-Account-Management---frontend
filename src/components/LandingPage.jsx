117596169ecba8ffbcdfa6c8cdeec6474000916c
Vikash Gurjar <vikashnagar2025@gmail.com>
2024-04-20 16:16:08 +0530

    Style : Add font family roboto

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -18,14 +18,14 @@ const imgs = [
     "stickers-f1c72e75_h8nt10.png",
     "envelopes-7a0199c5_qyaezn.png",
     "standy-201b66ef_j5imuj.png",
-    // "3dBoard-1528a010_vg34to.png",
-    // "laterHead-74d8d13e_xlyp28.png",
-    // "billBook-94423608_p9rb3b.png",
-    // "canopy-25ec1517_sziimr.png",
-    // "pamplet-1d28ad67_nnkb99.png",
-    // "visitingCard-b6b38756_mutaxy.png",
-    // "backLight-e844fd1e_yei077.png",
-    // "tShirt-729c4c96_th7lph.png"
+    "3dBoard-1528a010_vg34to.png",
+    "laterHead-74d8d13e_xlyp28.png",
+    "billBook-94423608_p9rb3b.png",
+    "canopy-25ec1517_sziimr.png",
+    "pamplet-1d28ad67_nnkb99.png",
+    "visitingCard-b6b38756_mutaxy.png",
+    "backLight-e844fd1e_yei077.png",
+    "tShirt-729c4c96_th7lph.png"
 ]
 
 const LandingPage = () => {
